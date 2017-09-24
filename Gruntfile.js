d695e7bd745ec4008ad58254ab25a48dec2a5a69
ninja_koder <grinders@tlen.pl>
2017-09-24 11:31:40 +0200

    Poprawione wcięcia w gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,24 +14,24 @@ module.exports = function(grunt) {
     },
 
     imagemin: {
-        dynamic: {
-            files: [{
-                expand: true,
-                cwd: 'images/',
-                src: ['**/*.{png,jpg,gif}'],
-                dest: 'images/build/'
-            }]
-        }
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'images/',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'images/build/'
+        }]
+      }
     },
 
     watch: {
-        scripts: {
-            files: ['sass/*.sass'],
-            tasks: ['sass'],
-            options: {
-                spawn: false,
-            },
-        }
+      scripts: {
+        files: ['sass/*.sass'],
+        tasks: ['sass'],
+        options: {
+          spawn: false,
+        },
+      }
     }
 
   });
